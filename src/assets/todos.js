a6120cb1d6a79ed6e4ddf2fc736aeba1baddc622
Nathan Sorkin <NateSrkn@gmail.com>
2018-08-10 01:56:49 -0400

    Fixing some clerical errors

--- src/assets/todos.js
+++ src/assets/todos.js
@@ -7,22 +7,22 @@ export default
    },
    {
     title: 'Mark a todo as complete',
-    description: "See the little circle? Click it when you've completed a task.",
+    description: "See the little box? Click it when you've completed a task. (Try it!)",
     isComplete: false
    },
    {
     title: 'Add a todo',
     description: 'I mean you can try it, just hit that plus button',
-    isComplete: false
+    isComplete: true
    },
    {
     title: 'Fix any errors',
     description: 'Misspell something? Change of plans? You can edit each one.',
-    isComplete: false,
+    isComplete: true,
    },
    {
        title: "Get rid of em'",
        description: "Don't want to see one anymore? Click the trash can and you're all done.",
-       isComplete: false,
+       isComplete: true,
    }
 ]
